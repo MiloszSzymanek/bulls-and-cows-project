6d3e7e8217232af3add68443e85e854175f4ff5d
admin <szymanekmilosz92@gmail.com>
2021-11-23 14:11:03 +0100

    demoversion

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// Bulls and Cows
-// Get library for user input
-// we need to keep the next line, so we can prompt the user for input
-//const prompt = require("prompt-sync")({ sigint: true });
+
 function getSecretNumber() {
   let fourth = Math.floor(Math.random() * 10);
   for (let i; (i = 1); i++) {
